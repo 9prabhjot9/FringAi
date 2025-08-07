143e9a16e8c9bb44493106d38f2e88399b01ae0a
Prabhjot Singh <ps459811@gmail.com>
2025-08-07 17:48:18 +0530

    first commit

---
+++ app/_components/AppSideBar.jsx
@@ -0,0 +1,75 @@
+"use client"
+import { Home,  Compass, Library, LogIn } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { usePathname } from "next/navigation"
+import { Button } from "@/components/ui/button"
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Discover",
+    url: "/discovery",
+    icon: Compass,
+  },
+  {
+    title: "Library",
+    url: "/library",
+    icon: Library,
+  },
+  {
+    title: "Sign In",
+    url: "# ",
+    icon: LogIn,
+  },
+]
+
+export function AppSidebar() {
+    const path = usePathname()
+  return (
+    <Sidebar >
+      <SidebarContent className='bg-[#dcfcf4]'>
+        <SidebarGroup>
+          <SidebarGroupLabel className='flex items-center py-13 '>
+            <img src={'/fringnew.png'} alt="logo" width={7000}  />
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton 
+                  asChild 
+                  className={`p-6 hover:bg-transparent hover:font-bold ${path?.includes(item.url) && 'font-bold'}`}>
+                    <a href={item.url}>
+                      <item.icon className="h-8 w-8"/>
+                      <span className="text-lg  ">{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+            <Button className={'text-black ml-5 w-40 mt-5 rounded-full bg-[#438c86]'}>Sign In</Button>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+            <div className="p-9 bg-[#dcfcf4]">
+                <h1 className="text-gray-500">Try Now</h1>
+                <p className="text-gray-400">Upgrade to premium for image upload and smarter AI</p>
+            </div>
+    </Sidebar>
+  )
+}
---
+++ app/_components/ChatInputBox.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+import React from "react"
+
+function ChatInputBox() {
+    return (
+        <div>
+            <Image src={'/logo.png'} alt="logo" width={100} height={100} />
+        </div>
+    )
+}
+
+export default ChatInputBox
