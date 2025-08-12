e3ce29de5a9e8419b50ba9f0f1415becc9f762cc
Prabhjot Singh <ps459811@gmail.com>
2025-08-12 16:59:56 +0530

    updated llm-model

--- app/api/llm-model/route.js
+++ app/api/llm-model/route.js
@@ -1,5 +1,17 @@
+import { Inngest } from "inngest";
+
 export async function POST(req){
     const{serachInput, serachResult, recordId} = await req.json()
 
-    await innegst
+    
+   const inngestRunId= await inngest.send({
+        name: "llm-model",
+        data: {
+            serachInput: serachInput,
+            serachResult: serachResult,
+            recordId: recordId
+        },
+    });
+
+    return NextResponse.json(inngestRunId)
 }
