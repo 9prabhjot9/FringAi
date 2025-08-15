e531f8a291a360e6c78e2e2159057c4939933fce
Prabhjot Singh <ps459811@gmail.com>
2025-08-15 17:04:00 +0530

    updated get-inngets route

--- app/api/get-inngest-status/route.jsx
+++ app/api/get-inngest-status/route.jsx
@@ -1,28 +1,23 @@
-import axios from "axios";
+import axios from "axios"
 
-export async function POST(req) {
-  try {
-    const { runId } = await req.json();
+export async function POST(req){
+    try{
+        const {runId} = await req.json()
 
-    if (!runId) {
-      return new Response(JSON.stringify({ error: "Missing runId" }), { status: 400 });
+        if(!runId){
+            return new Response(JSON.stringify({ error: "Missing runId"}), {status: 400})
+        }
+        const result = await axios.get(`${process.env.INNGEST_SERVER_HOST}/v1/events/${runId}/runs`,  {
+           headers: {
+                Authorization: `Bearer ${process.env.INNGEST_SIGNING_KEY}`,
+            },
+        })
+        return new Response(JSON.stringify(result.data),{
+            status: 200,
+            header: { "Content-Type": "application/josn"}
+        })
+    }catch(error){
+        console.error("Error in get-inngest-status", error.response?.data || error.message)
+        return new Response(JSON.stringify({error: error.message }), { status: 500})
     }
-
-    const result = await axios.get(
-      `${process.env.INNGEST_SERVER_HOST}/v1/events/${runId}/runs`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.INNGEST_SIGNING_KEY}`,
-        },
-      }
-    );
-
-    return new Response(JSON.stringify(result.data), {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    });
-  } catch (error) {
-    console.error("Error in get-inngest-status:", error.response?.data || error.message);
-    return new Response(JSON.stringify({ error: error.message }), { status: 500 });
-  }
 }
